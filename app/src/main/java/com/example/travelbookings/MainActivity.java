40066d8d89337b9204d5dcfa7226521ae40cc832
Kevin Siundu <kevin.siundu@equitybank.co.ke>
2020-07-02 02:25:56 +0300

    working on creating a deal

--- app/src/main/java/com/example/travelbookings/MainActivity.java
+++ app/src/main/java/com/example/travelbookings/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.travelbookings;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private EditText txtTitle, txtDescription, txtPrice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtTitle = findViewById(R.id.editTextTitle);
+        txtDescription = findViewById(R.id.editTextDescription);
+        txtPrice = findViewById(R.id.editTextPrice);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_menu:
+                saveDeals();
+                clean();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void clean() {
+         txtTitle.setText("");
+         txtPrice.setText("");
+         txtDescription.setText("");
+
+         txtTitle.requestFocus();
+    }
+
+    private void saveDeals() {
+        String title = txtTitle.getText().toString();
+        String desc = txtDescription.getText().toString();
+        String price = txtPrice.getText().toString();
+    }
+
 }
